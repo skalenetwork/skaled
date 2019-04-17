b20b0d8b9aa860abcaf714a4109a81a86ebc83d3
Sergiy Lavrynenko <sergiy@skalelabs.com>
2019-04-17 14:01:15 +0300

    SKALE-984 unit tests for HTTP, HTTPS, WS and WSS

---
+++ test/unittests/libskutils/test_skutils_dispatch.cpp
@@ -0,0 +1,10 @@
+#include "test_skutils_helper.h"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( SkUtils )
+BOOST_AUTO_TEST_SUITE( dispatch )
+
+BOOST_AUTO_TEST_CASE( simple ) {}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
