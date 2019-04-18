1fe472e2b38d7b027daf0b06a88763d2ae0fb44b
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-04-18 13:11:27 +0300

    SKALE-963 Add building and deploying of docker image to dockerhub

---
+++ scripts/skale_build/deploy_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+IMAGE_NAME=skalelabshub/schain:$VERSION
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push $IMAGE_NAME
