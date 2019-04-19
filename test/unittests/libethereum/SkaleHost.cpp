1af3659758cc7ec6cc884a1dc66227d2a78654c6
Dima Litvinov <dima.litvinov@skalelabs.com>
2019-04-19 13:08:55 +0300

    SKALE-773 Initial implementation of test/SkaleHost

--- test/unittests/libethereum/SkaleHost.cpp
+++ test/unittests/libethereum/SkaleHost.cpp
@@ -81,18 +81,19 @@ struct SkaleHostFixture : public TestOutputHelperFixture {
 
         gasPricer = make_shared< eth::TrivialGasPricer >( 0, DefaultGasPrice );
 
-        client = make_unique< Client >( chainParams, chainParams.networkID, gasPricer, tempDir.path() );
+        client =
+            make_unique< Client >( chainParams, chainParams.networkID, gasPricer, tempDir.path() );
         client->setAuthor( coinbase.address() );
 
         ConsensusTestStubFactory test_stub_factory;
         skaleHost = make_shared< SkaleHost >( *client, tq, &test_stub_factory );
         stub = test_stub_factory.result;
 
-        client->injectSkaleHost(skaleHost);
+        client->injectSkaleHost( skaleHost );
         client->startWorking();
 
         // make money
-        //dev::eth::simulateMining( *client, 1 );
+        dev::eth::simulateMining( *client, 1 );
     }
 
     TransactionQueue tq;
@@ -122,15 +123,93 @@ BOOST_AUTO_TEST_CASE( validTransaction ) {
     TransactionSkeleton ts = toTransactionSkeleton( json );
     ts = client->populateTransactionWithDefaults( ts );
     pair< bool, Secret > ar = accountHolder->authenticate( ts );
-    Transaction t( ts, ar.second );
-    RLPStream s;
-    t.streamRLP( s );
-    Json::Value signedTx = toJson( t, s.out() );
+    Transaction tx( ts, ar.second );
 
-    BOOST_REQUIRE( !signedTx["raw"].empty() );
+    RLPStream stream;
+    tx.streamRLP( stream );
 
-//    auto txHash = rpcClient->eth_sendRawTransaction( signedTx["raw"].asString() );
-//    BOOST_REQUIRE( !txHash.empty() );
+    h256 txHash = tx.sha3();
+
+    BOOST_REQUIRE_EQUAL( client->number(), 0 );
+    BOOST_REQUIRE_NO_THROW(
+        stub->createBlock( ConsensusExtFace::transactions_vector{stream.out()}, utcTime(), 1U ) );
+    BOOST_REQUIRE_EQUAL( client->number(), 1 );
+
+    TransactionHashes blockTransactions =
+        static_cast< Interface* >( client.get() )->transactionHashes( 1 );
+
+    BOOST_REQUIRE( blockTransactions.size() == 1 );
+    BOOST_REQUIRE( blockTransactions[0] == txHash );
 }
+
+BOOST_AUTO_TEST_CASE( transactionRlpBad ) {
+    bytes small_tx1 = bytes();
+    bytes small_tx2 = jsToBytes( "0x0011223344556677889900" );
+    bytes bad_tx1 = jsToBytes(
+        "0x0011223344556677889900112233445566778899001122334455667788990011223344556677889900112233"
+        "445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788"
+        "990011223344556677889900112233445566778899" );
+    bytes bad_tx2 = jsToBytes(
+        "0xf86c223344556677889900112233445566778899001122334455667788990011223344556677889900112233"
+        "445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788"
+        "990011223344556677889900112233445566778899" );
+
+    BOOST_REQUIRE_EQUAL( client->number(), 0 );
+    BOOST_REQUIRE_NO_THROW( stub->createBlock(
+        ConsensusExtFace::transactions_vector{small_tx1, small_tx2, bad_tx1, bad_tx2}, utcTime(),
+        1U ) );
+    BOOST_REQUIRE_EQUAL( client->number(), 1 );
+
+    TransactionHashes blockTransactions =
+        static_cast< Interface* >( client.get() )->transactionHashes( 1 );
+
+    BOOST_REQUIRE( blockTransactions.size() == 0 );
+}
+
+BOOST_AUTO_TEST_CASE( transactionSigZero ) {
+    // TODO find out how to create zero signature
+}
+
+BOOST_AUTO_TEST_CASE( transactionSigBad ) {
+    auto senderAddress = coinbase.address();
+    auto receiver = KeyPair::create();
+
+    Json::Value json;
+    json["from"] = toJS( senderAddress );
+    json["to"] = toJS( receiver.address() );
+    json["value"] = jsToDecimal( toJS( 10000 * dev::eth::szabo ) );
+
+    TransactionSkeleton ts = toTransactionSkeleton( json );
+    ts = client->populateTransactionWithDefaults( ts );
+    pair< bool, Secret > ar = accountHolder->authenticate( ts );
+    Transaction tx( ts, ar.second );
+
+    RLPStream stream;
+    tx.streamRLP( stream );
+    bytes data = stream.out();
+
+    // TODO try to spoil other fields
+    data[43] = 0x7f;      // spoil v
+
+    BOOST_REQUIRE_EQUAL( client->number(), 0 );
+    BOOST_REQUIRE_NO_THROW(
+        stub->createBlock( ConsensusExtFace::transactions_vector{data}, utcTime(), 1U ) );
+    BOOST_REQUIRE_EQUAL( client->number(), 1 );
+
+    TransactionHashes blockTransactions =
+        static_cast< Interface* >( client.get() )->transactionHashes( 1 );
+
+    BOOST_REQUIRE( blockTransactions.size() == 0 );
+}
+
+BOOST_AUTO_TEST_CASE( transactionGasSmall ) {}
+
+BOOST_AUTO_TEST_CASE( transactionNonceSmall ) {}
+
+BOOST_AUTO_TEST_CASE( transactionNonceBig ) {}
+
+BOOST_AUTO_TEST_CASE( transactionBalanceBad ) {}
+
+BOOST_AUTO_TEST_CASE( transactionGasBlockLimitExceeded ) {}
 
 BOOST_AUTO_TEST_SUITE_END()
