376c14388b91270dfdceb51b2dd329497489820f
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-06-07 16:42:14 +0300

    SKALE-1186 fix build

--- test/unittests/libethereum/SkaleHost.cpp
+++ test/unittests/libethereum/SkaleHost.cpp
@@ -45,7 +45,7 @@ public:
     }
     void createBlock( const ConsensusExtFace::transactions_vector& _approvedTransactions,
         uint64_t _timeStamp, uint64_t _blockID ) {
-        m_extFace.createBlock( _approvedTransactions, _timeStamp, _blockID );
+        m_extFace.createBlock( _approvedTransactions, _timeStamp, _blockID , 0  );
     }
 };
 
