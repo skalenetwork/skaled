1657347d257637f59801d600c2481893cc9a013c
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-06-26 18:04:25 +0300

    SKALE-1201 Add createDirectory unit test

--- test/unittests/libethcore/PrecompiledTest.cpp
+++ test/unittests/libethcore/PrecompiledTest.cpp
@@ -1575,4 +1575,20 @@ BOOST_AUTO_TEST_CASE( deleteFile ) {
 
 BOOST_AUTO_TEST_SUITE_END()
 
+BOOST_AUTO_TEST_CASE( createDirectory ) {
+    PrecompiledExecutor exec = PrecompiledRegistrar::executor( "createDirectory" );
+
+    Address ownerAddress = Address( "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF" );
+    std::string dirName = "test_dir";
+    boost::filesystem::path pathToDir = dev::getDataDir() / "filestorage" / ownerAddress.hex() / dirName;
+
+    std::string hexAddress = ownerAddress.hex();
+    hexAddress.insert( hexAddress.begin(), 64 - hexAddress.length(), '0' );
+    bytes in = fromHex( hexAddress + numberToHex( dirName.length() ) + stringToHex( dirName ) );
+    auto res = exec( bytesConstRef( in.data(), in.size() ) );
+    BOOST_REQUIRE( res.first );
+    BOOST_REQUIRE( boost::filesystem::exists(pathToDir));
+    remove( pathToDir.c_str() );
+}
+
 BOOST_AUTO_TEST_SUITE_END()
