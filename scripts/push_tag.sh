cb5554cff98f22937f61c1b43947000344d351c5
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-07-16 14:58:47 +0300

    Add push tag script

--- scripts/push_tag.sh
+++ scripts/push_tag.sh
@@ -3,10 +3,4 @@
 TAG=$1
 
 echo "Push tag $TAG"
-
-#VERSION=$1
-#DOCKER_USERNAME=$2
-#DOCKER_PASSWORD=$3
-#IMAGE_NAME=skalelabshub/schain:$VERSION
-
-#echo "Push image $IMAGE_NAME"
+git push origin $TAG
