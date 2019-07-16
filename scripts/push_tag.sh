9f76afd0be7d5d2a8ef2f6153c9700690ec36e36
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-07-16 13:47:25 +0300

    Debug

--- scripts/push_tag.sh
+++ scripts/push_tag.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-TAG=$1
+#TAG=$1
 
-echo "Push tag $TAG"
+#echo "Push tag $TAG"
+
+VERSION=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+IMAGE_NAME=skalelabshub/schain:$VERSION
+
+echo "Push image $IMAGE_NAME"
