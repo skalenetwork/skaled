a0f9605159113308dffdd0fc85acaca29c3aa737
Dima Litvinov <dima.litvinov@skalelabs.com>
2019-10-17 12:33:53 +0300

    SKALE-1504 Use equal (second) timestamp in different blocks

--- libethereum/Block.cpp
+++ libethereum/Block.cpp
@@ -146,7 +146,8 @@ void Block::resetCurrent( int64_t _timestamp ) {
     m_transactionSet.clear();
     m_currentBlock = BlockHeader();
     m_currentBlock.setAuthor( m_author );
-    m_currentBlock.setTimestamp( max( m_previousBlock.timestamp() + 1, _timestamp ) );
+    m_currentBlock.setTimestamp( _timestamp );  // max( m_previousBlock.timestamp() + 1, _timestamp
+                                                // ) );
     m_currentBytes.clear();
     sealEngine()->populateFromParent( m_currentBlock, m_previousBlock );
 
