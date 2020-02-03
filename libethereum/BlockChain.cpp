c9d171069297cf87fcd6a35a3597343f41333b00
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-02-03 12:30:25 +0200

    SKALE-2108 Fix transaction position always 0 in block

--- libethereum/BlockChain.cpp
+++ libethereum/BlockChain.cpp
@@ -803,6 +803,7 @@ ImportRoute BlockChain::insertBlockAndExtras( VerifiedBlockRef const& _block,
                     *i == _block.info.hash() ? _block.block : &( blockBytes = block( *i ) ) );
                 TransactionAddress ta;
                 ta.blockHash = tbi.hash();
+                ta.index = 0;
 
                 RLP txns_rlp = blockRLP[1];
 
@@ -812,6 +813,7 @@ ImportRoute BlockChain::insertBlockAndExtras( VerifiedBlockRef const& _block,
                     extrasWriteBatch->insert(
                         toSlice( sha3( ( *it ).data() ), ExtraTransactionAddress ),
                         ( db::Slice ) dev::ref( ta.rlp() ) );
+                    ++ta.index;
                 }
             }
 
