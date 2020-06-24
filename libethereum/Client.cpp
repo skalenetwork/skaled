33615504655464ad78c66ca6dc8114e0673d9492
Oleh Nikolaiev <oleg@skalelabs.com>
2020-06-24 08:54:04 +0300

    SKALE-2755 format

--- libethereum/Client.cpp
+++ libethereum/Client.cpp
@@ -981,13 +981,15 @@ ExecutionResult Client::call( Address const& _from, u256 _value, Address _dest, 
 
 void Client::updateHashes() {
     if ( this->last_snapshot_hashes.first == this->empty_str_hash ) {
-        this->last_snapshot_hashes.first = this->m_snapshotManager->getSnapshotHash( this->last_snapshoted_block );
+        this->last_snapshot_hashes.first =
+            this->m_snapshotManager->getSnapshotHash( this->last_snapshoted_block );
         return;
     }
     if ( this->last_snapshot_hashes.second != this->empty_str_hash ) {
         std::swap( this->last_snapshot_hashes.first, this->last_snapshot_hashes.second );
     }
-    this->last_snapshot_hashes.second = this->m_snapshotManager->getSnapshotHash( this->last_snapshoted_block );
+    this->last_snapshot_hashes.second =
+        this->m_snapshotManager->getSnapshotHash( this->last_snapshoted_block );
 }
 
 // new block watch
