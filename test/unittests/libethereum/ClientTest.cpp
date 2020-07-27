57de4c8facf92e3a068de021a0b2e0673360a1d3
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-07-27 17:28:06 +0300

    SKALE-2930 Fix test

--- test/unittests/libethereum/ClientTest.cpp
+++ test/unittests/libethereum/ClientTest.cpp
@@ -131,8 +131,17 @@ public:
         //            GasPricer >(),
         //                dir, dir, WithExisting::Kill, TransactionQueue::Limits{100000, 1024} ) );
 
+        // wait for 1st block - because it's always empty
+        std::promise< void > block_promise;
+        auto importHandler = m_ethereum->setOnBlockImport(
+            [&block_promise]( BlockHeader const& ) {
+                    block_promise.set_value();
+        } );
+
         m_ethereum->injectSkaleHost();
         m_ethereum->startWorking();
+
+        block_promise.get_future().wait();
 
         m_ethereum->setAuthor( coinbase.address() );
 
