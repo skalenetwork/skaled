fbf90dfaf8349809c97da1a6ce740079532bf766
Oleh Nikolaiev <oleg@skalelabs.com>
2021-08-10 16:09:56 +0300

    SKALE-4474 fix filestorage hash for snapshot

--- test/unittests/libethcore/PrecompiledTest.cpp
+++ test/unittests/libethcore/PrecompiledTest.cpp
@@ -1655,6 +1655,7 @@ BOOST_AUTO_TEST_CASE( getFileSize ) {
 }
 
 BOOST_AUTO_TEST_CASE( deleteFile ) {
+    BOOST_REQUIRE( boost::filesystem::exists( pathToFile.string() + "._hash" ) );
     PrecompiledExecutor exec = PrecompiledRegistrar::executor( "deleteFile" );
 
     bytes in = fromHex( hexAddress + numberToHex( fileName.length() ) + stringToHex( fileName ) );
@@ -1675,7 +1676,6 @@ BOOST_AUTO_TEST_CASE( createDirectory ) {
     auto res = exec( bytesConstRef( in.data(), in.size() ) );
     BOOST_REQUIRE( res.first );
     BOOST_REQUIRE( boost::filesystem::exists( pathToDir ) );
-    BOOST_REQUIRE( boost::filesystem::exists( pathToDir.string() + "._hash" ) );
     remove( pathToDir.c_str() );
 }
 
@@ -1691,7 +1691,6 @@ BOOST_AUTO_TEST_CASE( deleteDirectory ) {
     auto res = exec( bytesConstRef( in.data(), in.size() ) );
     BOOST_REQUIRE( res.first );
     BOOST_REQUIRE( !boost::filesystem::exists( pathToDir ) );
-    BOOST_REQUIRE( !boost::filesystem::exists( pathToDir.string() + "._hash" ) );
 }
 
 BOOST_AUTO_TEST_CASE( calculateFileHash ) {
@@ -1700,7 +1699,8 @@ BOOST_AUTO_TEST_CASE( calculateFileHash ) {
     std::string fileHashName = pathToFile.string() + "._hash";
 
     std::ofstream fileHash( fileHashName );
-    dev::h256 hash = dev::sha256( pathToFile.string() );
+    std::string relativePath = pathToFile.string().substr( pathToFile.string().find( "filestorage" ) );
+    dev::h256 hash = dev::sha256( relativePath );
     fileHash << hash;
 
     fileHash.close();
