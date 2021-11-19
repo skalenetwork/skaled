fc19d0250194f431ac52f2ecaf65554211396450
Dima Litvinov <dima.litvinov@skalelabs.com>
2021-11-19 18:19:15 +0400

    SKALE-4704 Do not process any more blocks if exit is initiated

--- libethereum/SkaleHost.cpp
+++ libethereum/SkaleHost.cpp
@@ -474,6 +474,11 @@ ConsensusExtFace::transactions_vector SkaleHost::pendingTransactions(
 void SkaleHost::createBlock( const ConsensusExtFace::transactions_vector& _approvedTransactions,
     uint64_t _timeStamp, uint64_t _blockID, u256 _gasPrice, u256 _stateRoot,
     uint64_t _winningNodeIndex ) try {
+
+    // ACHTUNG! This is important when node rotation is initiated! (not to process next block)
+    if(dev::ExitHandler::shouldExit())
+        return;
+
     //
     static std::atomic_size_t g_nCreateBlockTaskNumber = 0;
     size_t nCreateBlockTaskNumber = g_nCreateBlockTaskNumber++;
