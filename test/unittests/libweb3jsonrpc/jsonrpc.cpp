8b785d39b0297c6f900cd70a405e4bfff7b870cf
Oleh <oleg@skalelabs.com>
2021-12-30 17:57:06 +0200

    rename previous groups to node groups

--- test/unittests/libweb3jsonrpc/jsonrpc.cpp
+++ test/unittests/libweb3jsonrpc/jsonrpc.cpp
@@ -98,7 +98,7 @@ static std::string const c_genesisConfigString =
             "schainID": 1,
             "contractStorageLimit": 128,
             "emptyBlockIntervalMs": -1,
-            "previousGroups": {},
+            "nodeGroups": {},
             "nodes": [
                 { "nodeID": 1112, "ip": "127.0.0.1", "basePort": 1231, "schainIndex" : 1, "publicKey": "0xfa"}
             ]
