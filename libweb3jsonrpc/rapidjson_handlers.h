b476dde0f6dc56e1184ef55d014fd325a0a2d9b5
Dima Litvinov <dima.litvinov@skalelabs.com>
2022-02-18 21:09:22 +0200

    SKALE-4960 Code-restrucuring

---
+++ libweb3jsonrpc/rapidjson_handlers.h
@@ -0,0 +1,9 @@
+#ifndef RAPIDJSON_HANDLERS_H
+#define RAPIDJSON_HANDLERS_H
+
+#include <libskale/httpserveroverride.h>
+#include <libweb3jsonrpc/Eth.h>
+
+extern void inject_rapidjson_handlers( SkaleServerOverride::opts_t& serverOpts, dev::rpc::Eth* pEthFace );
+
+#endif // RAPIDJSON_HANDLERS_H
