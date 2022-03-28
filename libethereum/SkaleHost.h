c05391362694d1ff9113209762670cf324304d1d
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-03-28 18:34:50 +0000

    SKALE-5055 Add broadcastEnabled flag

--- libethereum/SkaleHost.h
+++ libethereum/SkaleHost.h
@@ -107,7 +107,7 @@ public:
 
     SkaleHost( dev::eth::Client& _client, const ConsensusFactory* _consFactory = nullptr,
         std::shared_ptr< InstanceMonitor > _instanceMonitor = nullptr,
-        const std::string& _gethURL = "" );
+        const std::string& _gethURL = "", bool _broadcastEnabled = true );
     virtual ~SkaleHost();
 
     void startWorking();
@@ -190,6 +190,7 @@ private:
     dev::eth::Client& m_client;
     dev::eth::TransactionQueue& m_tq;  // transactions ready to go to consensus
     std::shared_ptr< InstanceMonitor > m_instanceMonitor;
+    bool m_broadcastEnabled;
 
     dev::Logger m_debugLogger{dev::createLogger( dev::VerbosityDebug, "skale-host" )};
     dev::Logger m_traceLogger{dev::createLogger( dev::VerbosityTrace, "skale-host" )};
