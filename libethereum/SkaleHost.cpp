edf3dd9b420c2d1b93a22081d6bc7b1e637674ed
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-03-28 18:34:50 +0000

    SKALE-5055 Add broadcastEnabled flag

--- libethereum/SkaleHost.cpp
+++ libethereum/SkaleHost.cpp
@@ -211,10 +211,12 @@ void ConsensusExtImpl::terminateApplication() {
 }
 
 SkaleHost::SkaleHost( dev::eth::Client& _client, const ConsensusFactory* _consFactory,
-    std::shared_ptr< InstanceMonitor > _instanceMonitor, const std::string& _gethURL ) try
+    std::shared_ptr< InstanceMonitor > _instanceMonitor, const std::string& _gethURL, 
+    bool _broadcastEnabled ) try
     : m_client( _client ),
       m_tq( _client.m_tq ),
       m_instanceMonitor( _instanceMonitor ),
+      m_broadcastEnabled( _broadcastEnabled ),
       total_sent( 0 ),
       total_arrived( 0 ) {
     m_debugHandler = [this]( const std::string& arg ) -> std::string {
