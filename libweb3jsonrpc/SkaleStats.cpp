75fb88026f15933aab93e8c7f6ef1b45c1b74316
Sergiy Lavrynenko <sergiy@skalelabs.com>
2022-05-25 19:45:07 +0300

    code cleaunup

--- libweb3jsonrpc/SkaleStats.cpp
+++ libweb3jsonrpc/SkaleStats.cpp
@@ -1423,13 +1423,7 @@ static dev::bytes& stat_bytes_align_left( dev::bytes& vec, size_t cnt ) {
     return vec;
 }
 
-static bytes& stat_array_align_left( bytes& vec, size_t cnt ) {
-    while ( vec.size() < cnt )
-        vec.insert( vec.begin(), 0 );
-    return vec;
-}
-
-static bytes& stat_array_align_right( bytes& vec, size_t cnt ) {
+static dev::bytes& stat_array_align_right( dev::bytes& vec, size_t cnt ) {
     while ( vec.size() < cnt )
         vec.push_back( 0 );
     return vec;
@@ -1491,7 +1485,7 @@ static dev::bytes& stat_remove_leading_zeros( dev::bytes& vec ) {
 static dev::bytes& stat_append_hash_str_2_vec( dev::bytes& vec, const std::string& s ) {
     dev::u256 val( s );
     bytes v = dev::BMPBN::encode2vec< dev::u256 >( val, true );
-    stat_array_align_left( v, 32 );
+    stat_bytes_align_left( v, 32 );
     vec.insert( vec.end(), v.begin(), v.end() );
     return vec;
 }
