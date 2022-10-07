9d64bbbae042be5543b17b9e994edae5a1a61466
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-10-07 19:03:17 +0000

    Fix deadlock

--- libskale/RevertableFSPatch.cpp
+++ libskale/RevertableFSPatch.cpp
@@ -6,5 +6,5 @@ dev::eth::Client* RevertableFSPatch::g_client;
 
 bool RevertableFSPatch::isEnabled() {
     return g_client->chainParams().sChain.revertableFSPatchTimestamp >=
-           g_client->latestBlock().info().timestamp();
+           g_client->blockChain().info().timestamp();
 }
