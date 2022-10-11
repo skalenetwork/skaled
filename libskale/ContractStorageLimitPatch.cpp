ac0d762a1dcce383083e7d4476324969a3cb2d59
Oleh Nikolaiev <oleg@skalelabs.com>
2022-10-11 12:58:50 +0300

    fix nullptr in tests

--- libskale/ContractStorageLimitPatch.cpp
+++ libskale/ContractStorageLimitPatch.cpp
@@ -5,6 +5,10 @@
 dev::eth::Client* ContractStorageLimitPatch::g_client;
 
 bool ContractStorageLimitPatch::isEnabled() {
+    if ( !g_client ) {
+        // tests
+        return true;
+    }
     return g_client->chainParams().sChain.introduceChangesTimestamp <=
            g_client->blockChain().info().timestamp();
 }
