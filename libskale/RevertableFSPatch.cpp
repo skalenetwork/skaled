f30da1755881f346483a45fb462f5795119bcf79
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-10-19 14:56:31 +0000

    Update patch for testing

--- libskale/RevertableFSPatch.cpp
+++ libskale/RevertableFSPatch.cpp
@@ -5,6 +5,8 @@
 dev::eth::Client* RevertableFSPatch::g_client;
 
 bool RevertableFSPatch::isEnabled() {
+    if ( !g_client ) 
+        return false;
     return g_client->chainParams().sChain.revertableFSPatchTimestamp <=
            g_client->blockChain().info().timestamp();
 }
