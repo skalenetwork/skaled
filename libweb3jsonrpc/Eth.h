59d1cefefba189100cd3e9e7bb18479de5626a41
kladkogex <13399135+kladkogex@users.noreply.github.com>
2022-12-13 15:00:04 +0000

    1251 Clang reformat

--- libweb3jsonrpc/Eth.h
+++ libweb3jsonrpc/Eth.h
@@ -64,7 +64,7 @@ public:
     Eth( const std::string& configPath, eth::Interface& _eth, eth::AccountHolder& _ethAccounts );
 
     virtual RPCModules implementedModules() const override {
-        return RPCModules{RPCModule{"eth", "1.0"}};
+        return RPCModules{ RPCModule{ "eth", "1.0" } };
     }
 
     bool isEnabledTransactionSending() const;
@@ -153,14 +153,12 @@ protected:
 
     // a cache that maps the call request to the pair of response string and block number
     // for which the request has been executed
-    cache::lru_cache<string, string> m_callCache;
+    cache::lru_cache< string, string > m_callCache;
 
     // a cache that maps a transaction receipt to the block number where
     // the transaction was not yet ready
     // for which the request has been executed
-    cache::lru_cache<string, ptr<dev::eth::LocalisedTransactionReceipt>> m_receiptsCache;
-
-
+    cache::lru_cache< string, ptr< dev::eth::LocalisedTransactionReceipt > > m_receiptsCache;
 };
 
 }  // namespace rpc
