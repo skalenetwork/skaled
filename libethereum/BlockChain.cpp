b3519471aebe858e184eaeb5223ca85d25c0b493
kladkogex <13399135+kladkogex@users.noreply.github.com>
2023-01-03 18:45:20 +0000

    1261 Merge final

--- libethereum/BlockChain.cpp
+++ libethereum/BlockChain.cpp
@@ -177,8 +177,8 @@ string BlockChain::getChainDirName( const ChainParams& _cp ) {
 }
 
 BlockChain::BlockChain( ChainParams const& _p, fs::path const& _dbPath, bool _applyPatches,
-        WithExisting _we ) try : m_lastBlockHashes( new LastBlockHashes( *this ) ),
-                                 m_dbPath( _dbPath ) {
+    WithExisting _we ) try : m_lastBlockHashes( new LastBlockHashes( *this ) ),
+      m_dbPath( _dbPath ) {
     init( _p );
     open( _dbPath, _applyPatches, _we );
 } catch ( ... ) {
