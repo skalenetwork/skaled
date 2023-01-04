a940fe4e0df5831e72e3083a384421f858710e7d
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2023-01-04 18:11:15 +0000

    Disable patches for 0 ts

--- libskale/RevertableFSPatch.cpp
+++ libskale/RevertableFSPatch.cpp
@@ -4,5 +4,8 @@ time_t RevertableFSPatch::revertableFSPatchTimestamp;
 time_t RevertableFSPatch::lastBlockTimestamp;
 
 bool RevertableFSPatch::isEnabled() {
+    if ( revertableFSPatchTimestamp == 0 ) {
+        return false;
+    }
     return revertableFSPatchTimestamp <= lastBlockTimestamp;
 }
