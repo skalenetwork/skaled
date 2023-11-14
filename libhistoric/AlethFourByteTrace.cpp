33095f4fc11e845a9f7b21676fed4f466765587d
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2023-11-14 12:49:24 +0000

    #1664 adding 4byte and noop tracers

---
+++ libhistoric/AlethFourByteTrace.cpp
@@ -0,0 +1,41 @@
+/*
+Copyright (C) 2023-present, SKALE Labs
+
+This file is part of skaled.
+
+skaled is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+skaled is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with skaled.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "FunctionCall.h"
+#include "AlethStandardTrace.h"
+
+
+namespace dev {
+namespace eth {
+
+
+void eth::AlethStandardTrace::fourByteTracePrint(
+    ExecutionResult& , const HistoricState& , const HistoricState&  ) {
+    std::map<string, uint64_t> callMap;
+    m_topFunctionCall->collectFourByteTrace( callMap);
+    {
+        for (auto&& key : callMap) {
+            m_jsonTrace[key.first] = to_string(key.second);
+        }
+    }
+}
+
+
+}  // namespace eth
+}  // namespace dev
