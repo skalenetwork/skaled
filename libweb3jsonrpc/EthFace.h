88bdc87df101e47fffcaf3f93f258ec127a09ae9
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-12-07 18:02:50 +0000

    SKALED-1600 Make 2nd parameter of estimateGas optional

--- libweb3jsonrpc/EthFace.h
+++ libweb3jsonrpc/EthFace.h
@@ -212,14 +212,12 @@ public:
                                     jsonrpc::JSON_OBJECT, NULL ),
             &dev::rpc::EthFace::eth_syncingI );
         this->bindAndAddMethod( jsonrpc::Procedure( "eth_estimateGas", jsonrpc::PARAMS_BY_POSITION,
-                                    jsonrpc::JSON_STRING, "param1", jsonrpc::JSON_OBJECT, "param2",
                                     jsonrpc::JSON_STRING, NULL ),
             &dev::rpc::EthFace::eth_estimateGasI );
         this->bindAndAddMethod( jsonrpc::Procedure( "eth_chainId", jsonrpc::PARAMS_BY_POSITION,
                                     jsonrpc::JSON_STRING, NULL ),
             &dev::rpc::EthFace::eth_chainIdI );
     }
-
     inline virtual void eth_protocolVersionI( const Json::Value& request, Json::Value& response ) {
         ( void ) request;
         response = this->eth_protocolVersion();
@@ -420,6 +418,9 @@ public:
         response = this->eth_syncing();
     }
     inline virtual void eth_estimateGasI( const Json::Value& request, Json::Value& response ) {
+        if ( !request.isArray() || request.empty() )
+            BOOST_THROW_EXCEPTION(
+                jsonrpc::JsonRpcException( jsonrpc::Errors::ERROR_RPC_INVALID_PARAMS ) );
         response = this->eth_estimateGas( request[0u] );
     }
     inline virtual void eth_chainIdI( const Json::Value& request, Json::Value& response ) {
