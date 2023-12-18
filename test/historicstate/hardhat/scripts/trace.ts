c37a4b45fac55e7fc9284bda2b1e21dae2926b17
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2023-12-18 18:24:23 +0000

    #1664 fixing compatibiliti with geth default trace

--- test/historicstate/hardhat/scripts/trace.ts
+++ test/historicstate/hardhat/scripts/trace.ts
@@ -1,10 +1,10 @@
 import {mkdir, readdir, writeFileSync, readFile, unlink} from "fs";
-import {promises as fs} from 'fs';
+const fs = require('fs');
 import {existsSync} from "fs";
 import deepDiff, {diff} from 'deep-diff';
 import {expect} from "chai";
 import * as path from 'path';
-import {int} from "hardhat/internal/core/params/argumentTypes";
+import {int, string} from "hardhat/internal/core/params/argumentTypes";
 
 const OWNER_ADDRESS: string = "0x907cd0881E50d359bb9Fd120B1A5A143b1C97De6";
 const ZERO_ADDRESS: string = "0xO000000000000000000000000000000000000000";
@@ -46,7 +46,10 @@ async function deleteAndRecreateDirectory(dirPath: string): Promise<void> {
     try {
 
         // Recreate the directory
-        await fs.mkdir(dirPath, {recursive: true});
+
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath);
+        }
         console.log(`Directory recreated: ${dirPath}`);
     } catch (error) {
         console.error('An error occurred:', error);
@@ -54,7 +57,9 @@ async function deleteAndRecreateDirectory(dirPath: string): Promise<void> {
 }
 
 async function deleteDirectory(dirPath: string): Promise<void> {
-    const entries = await fs.readdir(dirPath, {withFileTypes: true});
+    if (!fs.existsSync(dirPath))
+        return;
+    const entries = fs.readdirSync(dirPath);
 
     // Iterate over directory contents
     for (const entry of entries) {
@@ -71,6 +76,31 @@ async function deleteDirectory(dirPath: string): Promise<void> {
 
     // Delete the now-empty directory
     await fs.rmdir(dirPath);
+}
+
+
+/**
+ * Replaces a string in a file.
+ * @param {string} _filePath - The path to the file.
+ * @param {string} _str1 - The string to be replaced.
+ * @param {string} _str2 - The string to replace with.
+ */
+async function replaceStringInFile(_filePath, _str1, _str2) {
+    // Read the file
+
+    let data = fs.readFileSync(_filePath, 'utf8');
+
+    console.log("Replacing ...");
+
+    // Replace the string
+    const transformedFile = data.replace(new RegExp(_str1, 'g'), _str2);
+
+    console.log("Replaced ...");
+
+        // Write the file
+    fs.writeFileSync(_filePath, transformedFile, 'utf8');
+
+
 }
 
 
@@ -194,10 +224,20 @@ async function callDebugTraceCall(_deployedContract: any, _tracer: string, _trac
 
     writeFileSync(SKALE_TRACES_DIR + _traceFileName, traceResult);
 
+    let deployedContractAddressLowerCase  = _deployedContract.address.toString().toLowerCase();
+    await replaceStringInFile(SKALE_TRACES_DIR + _traceFileName ,
+        deployedContractAddressLowerCase, TEST_CONTRACT_NAME + ".address");
+
+    let ownerAddressLowerCase  = OWNER_ADDRESS.toLowerCase();
+
+    await replaceStringInFile(SKALE_TRACES_DIR + _traceFileName ,
+        ownerAddressLowerCase,  "OWNER.address");
+
+
 }
 
 
-function readJSONFile(fileName: string): Promise<object> {
+async function readJSONFile(fileName: string): Promise<object> {
     return new Promise((resolve, reject) => {
         readFile(fileName, 'utf8', (err, data) => {
             if (err) {
@@ -264,6 +304,42 @@ async function verifyDefaultTraceAgainstGethTrace(_fileName: string) {
 }
 
 
+async function verifyPrestateTraceAgainstGethTrace(_fileName: string) {
+
+    const _expectedResultFileName = GETH_TRACES_DIR + _fileName;
+    const _actualResultFileName = SKALE_TRACES_DIR + _fileName;
+
+    let expectedResult = await readJSONFile(_expectedResultFileName)
+    let actualResult = await readJSONFile(_actualResultFileName)
+
+    const differences = deepDiff(expectedResult, actualResult)!;
+
+    let foundDiffs = false;
+
+
+    if (differences) {
+        differences.forEach((difference, index) => {
+            // do not print differences related to total gas in the account
+
+            if (difference.kind == "E" && difference.path!.length == 1) {
+                let key = difference.path![0];
+                if (key == "to" || key == "gas" || key == "gasUsed")
+                    return;
+            }
+
+            foundDiffs = true;
+
+            console.log(`Found difference (lhs is expected value) ${index + 1} at path:`, difference.path);
+            console.log(`Difference ${index + 1}:`, difference);
+        });
+    }
+
+
+    await expect(foundDiffs).to.be.eq(false)
+}
+
+
+
 async function verifyCallTraceAgainstGethTrace(_fileName: string) {
 
     const _expectedResultFileName = GETH_TRACES_DIR + _fileName;
@@ -315,6 +391,11 @@ async function verifyFourByteTraceAgainstGethTrace(_fileName: string) {
         differences.forEach((difference, index) => {
             // do not print differences related to total gas in the account
 
+            if (difference.kind == "E" && difference.path!.length == 2) {
+                if ( difference.path![0] == "difference.path![0]" || key == "gas" || key == "gasUsed")
+                    return;
+            }
+
             foundDiffs = true;
 
             console.log(`Found difference (lhs is expected value) ${index + 1} at path:`, difference.path);
@@ -346,6 +427,12 @@ async function verifyPrestateTraceAgainstGethTrace(_fileName: string) {
     if (differences) {
         differences.forEach((difference, index) => {
             // do not print differences related to total gas in the account
+
+            if (difference.kind == "E" && difference.path!.length == 2) {
+                if ( difference.path![0] == "OWNER.address" || difference.path![1] == "nonce")
+                    return;
+            }
+
 
             foundDiffs = true;
 
@@ -407,10 +494,13 @@ async function main(): Promise<void> {
 
     await verifyFourByteTraceAgainstGethTrace(TEST_CONTRACT_CALL_FOURBYTETRACER_FILE_NAME)
 
-
     await callDebugTraceCall(deployedContract, PRESTATE_TRACER, TEST_CONTRACT_CALL_PRESTATETRACER_FILE_NAME);
 
-    await verifyCallTraceAgainstGethTrace(TEST_CONTRACT_CALL_PRESTATETRACER_FILE_NAME)
+    // since deployed contract address changes from one test run to another we replace the
+    // contract address with contract name. This is done to be able to compare trace files.
+
+
+    await verifyPrestateTraceAgainstGethTrace(TEST_CONTRACT_CALL_PRESTATETRACER_FILE_NAME)
 
 }
 
