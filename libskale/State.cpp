e2de35358b08b9cbe1b99bc10a15199473b731e3
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2024-01-22 19:57:38 +0000

    #1767 enforce transactions in block and refactor for clarity

--- libskale/State.cpp
+++ libskale/State.cpp
@@ -1033,8 +1033,7 @@ std::pair< ExecutionResult, TransactionReceipt > State::execute( EnvInfo const& 
         strRevertReason = skutils::eth::call_error_message_2_str( res.output );
         if ( strRevertReason.empty() )
             strRevertReason = "EVM revert instruction without description message";
-        std::string strOut = cc::fatal( "Error message from eth_call():" ) + cc::error( " " ) +
-                             cc::warn( strRevertReason );
+        std::string strOut = "Error message from State::execute(): "  + strRevertReason;
         cerror << strOut;
     }
 
