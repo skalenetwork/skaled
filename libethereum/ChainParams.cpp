f0d66393401b17512fc071114262a427ac425c2f
Oleh Nikolaiev <oleg@skalelabs.com>
2024-01-29 13:58:01 +0000

    #1652 remove debug flag

--- libethereum/ChainParams.cpp
+++ libethereum/ChainParams.cpp
@@ -238,7 +238,6 @@ ChainParams ChainParams::loadConfig(
 
         if ( sChainObj.count( "multiTransactionMode" ) )
             s.multiTransactionMode = sChainObj.at( "multiTransactionMode" ).get_bool();
-        s.multiTransactionMode = true;
 
         if ( sChainObj.count( "revertableFSPatchTimestamp" ) )
             s.revertableFSPatchTimestamp = sChainObj.at( "revertableFSPatchTimestamp" ).get_int64();
