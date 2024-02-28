50de3ec68e2b873d362ad1a0a665518b7f6e12c5
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2024-02-28 13:02:29 +0000

    #1780 Used another approach

--- libweb3jsonrpc/DebugFace.h
+++ libweb3jsonrpc/DebugFace.h
@@ -6,6 +6,7 @@
 #define JSONRPC_CPP_STUB_DEV_RPC_DEBUGFACE_H_
 
 #include "ModularServer.h"
+#include "boost/throw_exception.hpp"
 
 namespace dev {
 namespace rpc {
@@ -17,9 +18,11 @@ public:
                                     jsonrpc::JSON_STRING, "param2", jsonrpc::JSON_INTEGER, "param3",
                                     jsonrpc::JSON_STRING, "param4", jsonrpc::JSON_INTEGER, NULL ),
             &dev::rpc::DebugFace::debug_accountRangeAtI );
-        this->bindAndAddMethod( jsonrpc::Procedure( "debug_traceTransaction",
-                                    jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_OBJECT, "param1",
-                                    jsonrpc::JSON_STRING, "param2", jsonrpc::JSON_OBJECT, NULL ),
+
+
+        this->bindAndAddMethod(
+            jsonrpc::Procedure( "debug_traceTransaction", jsonrpc::PARAMS_BY_POSITION,
+                jsonrpc::JSON_OBJECT, NULL ),
             &dev::rpc::DebugFace::debug_traceTransactionI );
         this->bindAndAddMethod(
             jsonrpc::Procedure( "debug_storageRangeAt", jsonrpc::PARAMS_BY_POSITION,
@@ -30,13 +33,13 @@ public:
         this->bindAndAddMethod( jsonrpc::Procedure( "debug_preimage", jsonrpc::PARAMS_BY_POSITION,
                                     jsonrpc::JSON_OBJECT, "param1", jsonrpc::JSON_STRING, NULL ),
             &dev::rpc::DebugFace::debug_preimageI );
-        this->bindAndAddMethod( jsonrpc::Procedure( "debug_traceBlockByNumber",
-                                    jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_OBJECT, "param1",
-                                    jsonrpc::JSON_STRING, "param2", jsonrpc::JSON_OBJECT, NULL ),
+        this->bindAndAddMethod(
+            jsonrpc::Procedure( "debug_traceBlockByNumber", jsonrpc::PARAMS_BY_POSITION,
+                jsonrpc::JSON_OBJECT, NULL ),
             &dev::rpc::DebugFace::debug_traceBlockByNumberI );
-        this->bindAndAddMethod( jsonrpc::Procedure( "debug_traceBlockByHash",
-                                    jsonrpc::PARAMS_BY_POSITION, jsonrpc::JSON_OBJECT, "param1",
-                                    jsonrpc::JSON_STRING, "param2", jsonrpc::JSON_OBJECT, NULL ),
+        this->bindAndAddMethod(
+            jsonrpc::Procedure( "debug_traceBlockByHash", jsonrpc::PARAMS_BY_POSITION,
+                jsonrpc::JSON_OBJECT, NULL ),
             &dev::rpc::DebugFace::debug_traceBlockByHashI );
         this->bindAndAddMethod( jsonrpc::Procedure( "debug_traceCall", jsonrpc::PARAMS_BY_POSITION,
                                     jsonrpc::JSON_OBJECT, "param1", jsonrpc::JSON_OBJECT, "param2",
@@ -103,10 +106,25 @@ public:
         response = this->debug_accountRangeAt( request[0u].asString(), request[1u].asInt(),
             request[2u].asString(), request[3u].asInt() );
     }
-    inline virtual void debug_traceTransactionI(
-        const Json::Value& request, Json::Value& response ) {
-        response = this->debug_traceTransaction( request[0u].asString(), request[1u] );
+
+    inline virtual Json::Value getTracer( const Json::Value& request ) {
+        if ( !request.isArray() || request.empty() || request.size() > 2 ) {
+            BOOST_THROW_EXCEPTION(
+                jsonrpc::JsonRpcException( jsonrpc::Errors::ERROR_RPC_INVALID_PARAMS ) );
+        }
+        if ( request.size() == 2 ) {
+            if ( !request[1u].isObject() ) {
+                BOOST_THROW_EXCEPTION(
+                    jsonrpc::JsonRpcException( jsonrpc::Errors::ERROR_RPC_INVALID_PARAMS ) );
+            }
+            return request[1u];
+
+        } else {
+            return { Json::objectValue };
+        }
     }
+
+
     inline virtual void debug_storageRangeAtI( const Json::Value& request, Json::Value& response ) {
         response = this->debug_storageRangeAt( request[0u].asString(), request[1u].asInt(),
             request[2u].asString(), request[3u].asString(), request[4u].asInt() );
@@ -114,13 +132,19 @@ public:
     inline virtual void debug_preimageI( const Json::Value& request, Json::Value& response ) {
         response = this->debug_preimage( request[0u].asString() );
     }
+
+    inline virtual void debug_traceTransactionI(
+        const Json::Value& request, Json::Value& response ) {
+        response = this->debug_traceTransaction( request[0u].asString(), getTracer( request ) );
+    }
+
     inline virtual void debug_traceBlockByNumberI(
         const Json::Value& request, Json::Value& response ) {
-        response = this->debug_traceBlockByNumber( request[0u].asString(), request[1u] );
+        response = this->debug_traceBlockByNumber( request[0u].asString(), getTracer( request ) );
     }
     inline virtual void debug_traceBlockByHashI(
         const Json::Value& request, Json::Value& response ) {
-        response = this->debug_traceBlockByHash( request[0u].asString(), request[1u] );
+        response = this->debug_traceBlockByHash( request[0u].asString(), getTracer(request) );
     }
     inline virtual void debug_traceCallI( const Json::Value& request, Json::Value& response ) {
         response = this->debug_traceCall( request[0u], request[1u].asString(), request[2u] );
