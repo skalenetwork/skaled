74f58a4917f85ec61e391aa915e46e8df912a227
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2024-04-04 16:48:06 +0000

    1583 Fix solidity version in test

--- test/historicstate/hardhat/hardhat.config.js
+++ test/historicstate/hardhat/hardhat.config.js
@@ -3,14 +3,27 @@ require('@openzeppelin/hardhat-upgrades');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.25",
 };
 
 /* Address 0x907cd0881E50d359bb9Fd120B1A5A143b1C97De6 */
 const INSECURE_PRIVATE_KEY = "bd200f4e7f597f3c2c77fb405ee7fabeb249f63f03f43d5927b4fa0c43cfe85e";
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    compilers: [
+      {
+        version: `0.8.25`,
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 15000
+          },
+          evmVersion: `shanghai`
+        }
+      },
+    ],
+  },
   networks: {
     skaled: {
       url: `http://localhost:1234`,
