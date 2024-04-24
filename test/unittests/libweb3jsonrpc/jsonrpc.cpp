4c2d7af879f9cf4f129997661ea44ca0773ec5ea
Oleh Nikolaiev <oleg@skalelabs.com>
2024-04-24 15:44:51 +0100

    #1719 fix tests

--- test/unittests/libweb3jsonrpc/jsonrpc.cpp
+++ test/unittests/libweb3jsonrpc/jsonrpc.cpp
@@ -3111,7 +3111,7 @@ BOOST_AUTO_TEST_CASE( eip1559RpcMethods ) {
     for (Json::Value::ArrayIndex i = 0; i < blockCnt; ++i) {
         BOOST_REQUIRE( feeHistory["baseFeePerGas"][i].isString() );
         std::string estimatedBaseFeePerGas = EIP1559TransactionsPatch::isEnabledWhen(
-                    fixture.client->blockInfo( bn - i - 1 ).timestamp() ) ? toJS( fixture.client->gasBidPrice( bn - i ) ) : toJS( 0 );
+                    fixture.client->blockInfo( bn - i - 1 ).timestamp() ) ? toJS( fixture.client->gasBidPrice( bn - i - 1 ) ) : toJS( 0 );
         BOOST_REQUIRE( feeHistory["baseFeePerGas"][i].asString() == estimatedBaseFeePerGas );
         BOOST_REQUIRE_GT( feeHistory["gasUsedRatio"][i].asDouble(), 0 );
         BOOST_REQUIRE_GT( 1, feeHistory["gasUsedRatio"][i].asDouble() );
