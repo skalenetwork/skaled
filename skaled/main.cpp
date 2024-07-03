ae877bac294515b2aa7b72d5f685993ec52d902e
Oleh Nikolaiev <oleg@skalelabs.com>
2024-07-03 11:52:13 +0100

    IS 968 fix sleep

--- skaled/main.cpp
+++ skaled/main.cpp
@@ -1649,7 +1649,7 @@ int main( int argc, char** argv ) try {
                 // sleep before send skale_getSnapshot again - will receive error
                 clog( VerbosityInfo, "main" )
                     << std::string( "Will sleep for 60 seconds before downloading 0 snapshot" );
-                sleep( chainParams.sChain.snapshotDownloadTimeout );
+                sleep( 60 );
 
                 downloadAndProccessSnapshot(
                     snapshotManager, chainParams, urlToDownloadSnapshotFrom, false );
