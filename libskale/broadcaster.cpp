a2f8ca26e9cb8ff0bffb88de61070a5515d7fec9
Oleh Nikolaiev <oleg@skalelabs.com>
2024-07-29 18:19:53 +0100

    remove heartbeat

--- libskale/broadcaster.cpp
+++ libskale/broadcaster.cpp
@@ -90,13 +90,6 @@ void* ZmqBroadcaster::server_socket() const {
     if ( !m_zmq_server_socket ) {
         m_zmq_server_socket = zmq_socket( m_zmq_context, ZMQ_PUB );
 
-        int val = 15000;
-        zmq_setsockopt( m_zmq_server_socket, ZMQ_HEARTBEAT_IVL, &val, sizeof( val ) );
-        val = 3000;
-        zmq_setsockopt( m_zmq_server_socket, ZMQ_HEARTBEAT_TIMEOUT, &val, sizeof( val ) );
-        val = 60000;
-        zmq_setsockopt( m_zmq_server_socket, ZMQ_HEARTBEAT_TTL, &val, sizeof( val ) );
-
         val = 16;
         zmq_setsockopt( m_zmq_server_socket, ZMQ_SNDHWM, &val, sizeof( val ) );
 
