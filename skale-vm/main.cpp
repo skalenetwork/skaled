c319ded0f68ab214b040c247691486778ba64d88
Dima Litvinov <dima.litvinov@skalelabs.com>
2024-08-08 17:27:06 +0100

    SKALED-1935 Fix skale-vm build

--- skale-vm/main.cpp
+++ skale-vm/main.cpp
@@ -286,17 +286,18 @@ int main( int argc, char** argv ) {
         }  // Ignore decoding errors.
     }
 
-    unique_ptr< SealEngineFace > se( ChainParams( genesisInfo( networkName ) ).createSealEngine() );
+    ChainParams chainParams( genesisInfo( networkName ) );
     LastBlockHashes lastBlockHashes;
-    EnvInfo const envInfo(
-        blockHeader, lastBlockHashes, 0 /* gasUsed */, se->chainParams().chainID );
+    EnvInfo const envInfo( blockHeader, lastBlockHashes, 0 /*_committedBlockTimestamp*/,
+        0 /* gasUsed */, chainParams.chainID );
+    EVMSchedule evmSchedule = chainParams.makeEvmSchedule( 0, envInfo.number() );
 
     Transaction t;
     Address contractDestination( "1122334455667788991011121314151617181920" );
     if ( !code.empty() ) {
         // Deploy the code on some fake account to be called later.
         Account account( 0, 0 );
-        auto const latestVersion = se->evmSchedule( envInfo.number() ).accountVersion;
+        auto const latestVersion = evmSchedule.accountVersion;
         account.setCode( bytes{ code }, latestVersion );
         std::unordered_map< Address, Account > map;
         map[contractDestination] = account;
@@ -307,10 +308,12 @@ int main( int argc, char** argv ) {
         // data.
         t = Transaction( value, gasPrice, gas, data, 0 );
 
+    t.ignoreExternalGas();  // for tests
+
     state.addBalance( sender, value );
 
-    // HACK 0 here is for gasPrice
-    Executive executive( state, envInfo, *se, 0 );
+    // HACK 1st 0 here is for gasPrice
+    Executive executive( state, envInfo, chainParams, 0, 0 );
     ExecutionResult res;
     executive.setResultRecipient( res );
     t.forceSender( sender );
@@ -346,9 +349,8 @@ int main( int argc, char** argv ) {
     bytes output = std::move( res.output );
 
     if ( mode == Mode::Statistics ) {
-        cout << "Gas used: " << res.gasUsed << " (+"
-             << t.baseGasRequired( se->evmSchedule( envInfo.number() ) ) << " for transaction, -"
-             << res.gasRefunded << " refunded)\n";
+        cout << "Gas used: " << res.gasUsed << " (+" << t.baseGasRequired( evmSchedule )
+             << " for transaction, -" << res.gasRefunded << " refunded)\n";
         cout << "Output: " << toHex( output ) << "\n";
         LogEntries logs = executive.logs();
         cout << logs.size() << " logs" << ( logs.empty() ? "." : ":" ) << "\n";
