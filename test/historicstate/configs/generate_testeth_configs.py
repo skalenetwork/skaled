bf3cb92810eb5b83f22c55f862c759419a977193
kladkogex <13399135+kladkogex@users.noreply.github.com>
2024-09-26 21:14:11 +0100

    1545 adding 4 node files

--- test/historicstate/configs/generate_testeth_configs.py
+++ test/historicstate/configs/generate_testeth_configs.py
@@ -3,7 +3,17 @@ import json
 from deps.boost_1_68_0.libs.mpl.preprocessed.pp import pretty
 
 
-def generate_config_file(_schain_index, _node_id, _base_port):
+def calculate_base_port_from_index(_index):
+    return 1231 + (_index - 1) * 100
+
+
+def calculate_node_id(_chain_size, _index):
+    node_id_start = _chain_size * 100
+    return node_id_start + _index
+
+
+def generate_config_file(_chain_size, _schain_index):
+    node_id = calculate_node_id(_chain_size, _schain_index)
     try:
         with open('testeth_config.json', 'r') as file:
             data = json.load(file)
@@ -12,32 +22,34 @@ def generate_config_file(_schain_index, _node_id, _base_port):
     except json.JSONDecodeError:
         print("Error parsing JSON. Please check the JSON structure.")
 
-    data["skaleConfig"]["nodeInfo"]["nodeID"] = _node_id
-    data["skaleConfig"]["nodeInfo"]["basePort"] = _base_port
-    data["skaleConfig"]["nodeInfo"]["httpRpcPort"] = _base_port + 3
-    data["skaleConfig"]["nodeInfo"]["wsRpcPort"] = _base_port + 2
-    data["skaleConfig"]["nodeInfo"]["wssRpcPort"] = _base_port + 7
+    data["skaleConfig"]["nodeInfo"]["nodeID"] = node_id
+    base_port = calculate_base_port_from_index(_schain_index)
+    data["skaleConfig"]["nodeInfo"]["basePort"] = base_port
+    data["skaleConfig"]["nodeInfo"]["httpRpcPort"] = base_port + 3
+    data["skaleConfig"]["nodeInfo"]["wsRpcPort"] = base_port + 2
+    data["skaleConfig"]["nodeInfo"]["wssRpcPort"] = base_port + 7
     nodes_array = []
-    nodes_array.append(
-        {"basePort": _base_port, "ip": "127.0.0.1", "nodeID": _node_id, "publicKey": "", "schainIndex": _schain_index})
+    for index in range(1, _chain_size + 1):
+        nodes_array.append(
+            {"basePort": calculate_base_port_from_index(index), "ip": "127.0.0.1",
+             "nodeID": calculate_node_id(_chain_size, index), "publicKey": "",
+             "schainIndex": index})
     data["skaleConfig"]["sChain"]["nodes"] = nodes_array
-    data["skaleConfig"] ["nodeInfo"]["db-path"] = f'/tmp/test_eth_{schain_index}_of_{chain_size}'
+    data["skaleConfig"]["nodeInfo"]["db-path"] = f'/tmp/test_eth_{_schain_index}_of_{_chain_size}'
 
     # Convert Python dictionary to a pretty-printed JSON string
     pretty_json = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
-
-    print(pretty_json)
-
     # Print the nicely formatted JSON
     # Write the JSON string to the file
-    with open(f'test_{schain_index}_of_{chain_size}.json', 'w') as file:
+    with open(f'test_{_schain_index}_of_{_chain_size}.json', 'w') as file:
         file.write(pretty_json)
 
 
-chain_size = 1
-node_id_start = chain_size * 100
+def generate_test_configs_set(_chain_size):
+    node_id_start = _chain_size * 100
+    for schain_index in range(1, _chain_size + 1):
+        generate_config_file(_chain_size, schain_index)
 
-for schain_index in range(1, chain_size + 1):
-    node_id = node_id_start + schain_index
-    base_port = 1231 + (schain_index - 1) * 100
-    generate_config_file(schain_index, node_id, base_port)
+
+generate_test_configs_set(4)
+generate_test_configs_set(16)
