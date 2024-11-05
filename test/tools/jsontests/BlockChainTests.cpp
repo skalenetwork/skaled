f99e3a2a5ecbd4fa2c4068e13f52a72c56d82ad4
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2024-11-05 12:00:47 +0000

    1545 Fixing tests

--- test/tools/jsontests/BlockChainTests.cpp
+++ test/tools/jsontests/BlockChainTests.cpp
@@ -1018,12 +1018,8 @@ BOOST_FIXTURE_TEST_SUITE( BlockchainTests, bcTestFixture )
 BOOST_AUTO_TEST_CASE( bcStateTests ) {}
 BOOST_AUTO_TEST_CASE( bcBlockGasLimitTest ) {}
 BOOST_AUTO_TEST_CASE( bcGasPricerTest ) {}
-// Commenting this out as we do not support this
-//BOOST_AUTO_TEST_CASE( bcInvalidHeaderTest ) {}
 BOOST_AUTO_TEST_CASE( bcUncleHeaderValidity ) {}
 // Commenting this out as we do not support this
-//BOOST_AUTO_TEST_CASE( bcInvalidHeaderTest ) {}
-BOOST_AUTO_TEST_CASE( bcUncleTest ) {}
 BOOST_AUTO_TEST_CASE( bcValidBlockTest ) {}
 BOOST_AUTO_TEST_CASE( bcWalletTest,
                          *boost::unit_test::precondition( dev::test::run_not_express ) ) {}
