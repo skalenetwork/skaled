4edc50c082729419294fabddbb19e7dab7aa2e7d
Oleh Nikolaiev <oleg@skalelabs.com>
2025-01-17 11:04:30 +0000

    remove parallel options for liblzma

--- deps/build.sh
+++ deps/build.sh
@@ -673,7 +673,7 @@ then
         fi
         echo -e "${COLOR_INFO}building it${COLOR_DOTS}...${COLOR_RESET}"
         cd liblzma
-        eval "$MAKE" "${PARALLEL_MAKE_OPTIONS}"
+        eval "$MAKE"
         eval "$MAKE" "${PARALLEL_MAKE_OPTIONS}" install
         cd ..
         cd "$SOURCES_ROOT"
