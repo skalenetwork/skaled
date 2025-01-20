aa66c996df6765137265e358b1d28d9ae513b040
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-01-20 15:39:58 +0000

    1545 Second pull request - fix test

--- libskale/SkaleDebug.cpp
+++ libskale/SkaleDebug.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 
 
-std::atomic< bool > SkaleDebugInterface::g_isEnabled = true;
+std::atomic< bool > SkaleDebugInterface:: g_isEnabled = false;
 
 #define CHECK_ENABLED                          \
     if ( !SkaleDebugInterface::g_isEnabled ) { \
