77c9cafc1881181a97d9cdce9e45c842d37a3dce
Oleh Nikolaiev <oleg@skalelabs.com>
2025-01-23 13:15:13 +0000

    fix build deps

--- deps/build.sh
+++ deps/build.sh
@@ -673,6 +673,7 @@ then
         fi
         echo -e "${COLOR_INFO}building it${COLOR_DOTS}...${COLOR_RESET}"
         cd liblzma
+        # remove parallel options here as they cause failures
         eval "$MAKE"
         eval "$MAKE" "${PARALLEL_MAKE_OPTIONS}" install
         cd ..
