860feef5c49d4c09a320f6ee440148a14068cbee
badrogger <ivan@skalelabs.com>
2025-04-14 19:03:45 +0000

    IS-1158 Add safeSetAuthor

--- libethereum/Block.h
+++ libethereum/Block.h
@@ -121,6 +121,13 @@ public:
         resetCurrent();
     }
 
+    /// Set the author address for any transactions we do and rewards we get.
+    /// No reset of current block
+    void safeSetAuthor( Address const& _id ) {
+        m_author = _id;
+        m_currentBlock.setAuthor( m_author );
+    }
+
     /// Note the fact that this block is being used with a particular chain.
     /// Call this before using any non-const methods.
     void noteChain( BlockChain const& _bc );
