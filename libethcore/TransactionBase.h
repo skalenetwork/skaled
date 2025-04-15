a4f722772367dfec4e4c5e3d2d0efa1394f8dc5d
Oleh Nikolaiev <oleg@skalelabs.com>
2025-04-15 12:04:34 +0100

    IS 1162 format

--- libethcore/TransactionBase.h
+++ libethcore/TransactionBase.h
@@ -327,8 +327,8 @@ protected:
     u256 m_maxFeePerGas;          ///< The maximum fee per gas. Only valid for type2 txns
 
 #ifdef BITE
-    std::shared_ptr< bytes > m_decryptedData = nullptr;  ///< Transaction data that was decrypted in BITE
-                                                         ///< protocol
+    std::shared_ptr< bytes > m_decryptedData = nullptr;  ///< Transaction data that was decrypted in
+                                                         ///< BITE protocol
 #endif
 
     TransactionType m_txType = TransactionType::Legacy;
