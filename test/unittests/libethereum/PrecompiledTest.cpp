5e10e9f1ba0842d4c042bf9a1417d0c739122fa2
badrogger <ivan@skalelabs.com>
2025-04-17 14:46:00 +0000

    IS-1158 Switch owners in PrecompiledTests config

--- test/unittests/libethereum/PrecompiledTest.cpp
+++ test/unittests/libethereum/PrecompiledTest.cpp
@@ -1671,7 +1671,7 @@ static std::string const genesisInfoSkaleConfigTest = R"(
         "nodes": [
           { "nodeID": 1112, "owner": "0x0E7d7F1D34a502bD609542576941C3FCc087c588", "ip": "127.0.0.1", "basePort": )" +
         std::to_string( rand_port ) +
-        R"(, "schainIndex" : 1, "publicKey": "0xfa", "owner": "0x21abd6db4e347b4e8c937c1c8370e4b5ed3f0dd3db69cbdb7a38e1e50b1b82fc"}
+        R"(, "schainIndex" : 1, "publicKey": "0xfa" }
         ]
     }
   },
@@ -1728,7 +1728,7 @@ BOOST_AUTO_TEST_CASE( getConfigVariable ) {
     auto res = exec( bytesConstRef( in.data(), in.size() ) );
 
     BOOST_REQUIRE( res.first );
-    BOOST_REQUIRE( dev::fromBigEndian<dev::u256>( res.second ) == 30 );
+    BOOST_REQUIRE( dev::fromBigEndian<dev::u256>( res.second ) == 26 );
 
     input = stringToHex( "skaleConfig.sChain.nodes.0.schainIndex" );
     input = input.substr(0, 76); // remove 0s in the end
