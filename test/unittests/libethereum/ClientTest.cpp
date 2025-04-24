82e5567f3f3394e50d8906839095d365d8020df7
badrogger <ivan@skalelabs.com>
2025-04-24 19:49:21 +0100

    IS-1158 Fix ClientSnapshotsTest

--- test/unittests/libethereum/ClientTest.cpp
+++ test/unittests/libethereum/ClientTest.cpp
@@ -1032,7 +1032,7 @@ static std::string const c_skaleConfigString = R"E(
             "snapshotIntervalSec": 5,
             "emptyBlockIntervalMs": 4000,
             "nodes": [
-              { "nodeID": 1112, "ip": "127.0.0.1", "basePort": )E"+std::to_string( rand_port ) + R"E(, "ip6": "::1", "basePort6": 1231, "schainIndex" : 1, "publicKey" : "0xfa"}
+              { "nodeID": 1112, "owner": "0x0E7d7F1D34a502bD609542576941C3FCc087c588", "ip": "127.0.0.1", "basePort": )E"+std::to_string( rand_port ) + R"E(, "ip6": "::1", "basePort6": 1231, "schainIndex" : 1, "publicKey" : "0xfa"}
             ]
         }
     },
