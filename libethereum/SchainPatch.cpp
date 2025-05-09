5f35dcc86dda8ba5c38898ff5524267fd7c1b60b
Oleh Nikolaiev <oleg@skalelabs.com>
2025-05-09 18:49:40 +0100

    IS 1195 format

--- libethereum/SchainPatch.cpp
+++ libethereum/SchainPatch.cpp
@@ -89,12 +89,18 @@ std::string getPatchNameForEnum( SchainPatchEnum _enumValue ) {
 }
 
 #ifdef MIRAGE
-const std::unordered_set< SchainPatchEnum > SchainPatch::preEnabledForMIRAGE = { SchainPatchEnum::CorrectForkInPowPatch,
-                                           SchainPatchEnum::ContractStorageZeroValuePatch, SchainPatchEnum::PushZeroPatch, SchainPatchEnum::ContractStoragePatch, SchainPatchEnum::StorageDestructionPatch,
-                                           SchainPatchEnum::SkipInvalidTransactionsPatch, SchainPatchEnum::VerifyDaSigsPatch, SchainPatchEnum::FastConsensusPatch, SchainPatchEnum::EIP1559TransactionsPatch,
-                                           SchainPatchEnum::VerifyBlsSyncPatch, SchainPatchEnum::ClearPartialReceiptsPatch, SchainPatchEnum::InvalidTransactionFormatPatch };
-const std::unordered_set< SchainPatchEnum > SchainPatch::preDisabledForMIRAGE = { SchainPatchEnum::RevertableFSPatch, SchainPatchEnum::PowCheckPatch,
-        SchainPatchEnum::FlexibleDeploymentPatch, SchainPatchEnum::ExternalGasPatch };
+const std::unordered_set< SchainPatchEnum > SchainPatch::preEnabledForMIRAGE = {
+    SchainPatchEnum::CorrectForkInPowPatch, SchainPatchEnum::ContractStorageZeroValuePatch,
+    SchainPatchEnum::PushZeroPatch, SchainPatchEnum::ContractStoragePatch,
+    SchainPatchEnum::StorageDestructionPatch, SchainPatchEnum::SkipInvalidTransactionsPatch,
+    SchainPatchEnum::VerifyDaSigsPatch, SchainPatchEnum::FastConsensusPatch,
+    SchainPatchEnum::EIP1559TransactionsPatch, SchainPatchEnum::VerifyBlsSyncPatch,
+    SchainPatchEnum::ClearPartialReceiptsPatch, SchainPatchEnum::InvalidTransactionFormatPatch
+};
+const std::unordered_set< SchainPatchEnum > SchainPatch::preDisabledForMIRAGE = {
+    SchainPatchEnum::RevertableFSPatch, SchainPatchEnum::PowCheckPatch,
+    SchainPatchEnum::FlexibleDeploymentPatch, SchainPatchEnum::ExternalGasPatch
+};
 #endif
 
 void SchainPatch::init( const dev::eth::ChainOperationParams& _cp ) {
