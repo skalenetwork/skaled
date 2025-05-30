434645cdde1651d749426127a5bb973a22232e45
badrogger <ivan@skalelabs.com>
2025-05-30 13:43:11 +0100

    IS-1199 Fix formatter

--- libethereum/ClientBase.cpp
+++ libethereum/ClientBase.cpp
@@ -123,11 +123,9 @@ std::pair< u256, ExecutionResult > ClientBase::estimateGas( Address const& _from
             upperBound = c_maxGasEstimate;
         int64_t lowerBound;
 #ifdef MIRAGE
-        lowerBound = Transaction::baseGasRequired(
-            !_dest, &_data,
+        lowerBound = Transaction::baseGasRequired( !_dest, &_data,
             bc().sealEngine()->chainParams().makeEvmSchedule(
-            bc().info().timestamp(), bc().number() )
-        );
+                bc().info().timestamp(), bc().number() ) );
 #else
         if ( CorrectForkInPowPatch::isEnabledInWorkingBlock() )
             lowerBound = Transaction::baseGasRequired( !_dest, &_data,
