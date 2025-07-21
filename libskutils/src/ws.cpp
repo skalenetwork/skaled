f13a3b0fa3adec842eb60346890abc64200793af
Oleh Nikolaiev <oleg@skalelabs.com>
2025-07-21 10:39:56 +0100

    format

--- libskutils/src/ws.cpp
+++ libskutils/src/ws.cpp
@@ -2854,7 +2854,9 @@ server::server( basic_network_settings* pBNS )
         onLogMessage( eWSLMT, strMessage );
     };
 }
-server::~server() { close(); }
+server::~server() {
+    close();
+}
 
 nlohmann::json server::toJSON() const {
     nlohmann::json jo = nlohmann::json::object();
