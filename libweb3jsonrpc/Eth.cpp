5d21b1f6284f854f36d17ec1ab736871ee7dfb35
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2025-07-24 16:35:48 +0000

    #2239 Add dencunOpcodesInTransaction test

--- libweb3jsonrpc/Eth.cpp
+++ libweb3jsonrpc/Eth.cpp
@@ -505,7 +505,8 @@ string Eth::eth_call( TransactionSkeleton& t, string const&
                 JsonRpcException( REVERT_RPC_ERROR_CODE, strRevertReason, output ) );
         }
 
-        throw std::logic_error( strRevertReason );
+        BOOST_THROW_EXCEPTION(
+            JsonRpcException( REVERT_RPC_ERROR_CODE, strRevertReason ) );
     }
 #endif
 
